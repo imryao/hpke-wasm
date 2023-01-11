522e643419dfd544132a975b233dca0b15474da1
mryao <mryao@pku.edu.cn>
2023-01-11 21:41:03 +0800

    add pkr

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use chacha20poly1305::{AeadInPlace, ChaCha20Poly1305, Key, KeyInit, Nonce};
 use hkdf::Hkdf;
-use hpke::{Deserializable, Kem as KemTrait, OpModeS, PskBundle, Serializable};
+use hpke::{aead::Aead, Deserializable, kdf::Kdf, Kem as KemTrait, OpModeS, PskBundle, Serializable};
 use rand::{rngs::StdRng, SeedableRng};
 use sha2::Sha256;
 use wasm_bindgen::prelude::*;
@@ -11,19 +11,15 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-const _KEY_ID: u8 = 1;
-const PKR_BYTES: [u8; 32] = [31, 177, 33, 184, 17, 76, 225, 69, 155, 115, 196, 223, 218, 88, 21, 37, 221, 142, 172, 242, 10, 25, 103, 138, 254, 76, 169, 12, 52, 134, 227, 36];
-
 type KemType = hpke::kem::X25519HkdfSha256;
 type KdfType = hpke::kdf::HkdfSha256;
 type AeadType = hpke::aead::ChaCha20Poly1305;
 
-const HDR: [u8; 7] = [0x01, 0x00, 0x20, 0x00, 0x01, 0x00, 0x03];
-const INFO: [u8; 29] = [109, 101, 115, 115, 97, 103, 101, 47, 98, 104, 116, 116, 112, 32, 114, 101, 113, 117, 101, 115, 116, 0, 0x01, 0x00, 0x20, 0x00, 0x01, 0x00, 0x03];
-
 #[wasm_bindgen]
 pub fn s( // s for seal
           s: &[u8], // s for seed
+          d: u8, // d for key_id, key identifier
+          r: &[u8], // r for pkr, public key of recipient
           p: &[u8], // p for pt, plaintext
           k: &[u8], // k for psk, pre-shared key
           i: &[u8], // i for psk_id, pre-shared key identifier
@@ -34,19 +30,19 @@ pub fn s( // s for seal
     //              encode(2, kem_id),
     //              encode(2, kdf_id),
     //              encode(2, aead_id))
-    // let hdr = [
-    //     _KEY_ID.to_be_bytes().as_slice(),
-    //     KemType::KEM_ID.to_be_bytes().as_slice(),
-    //     KdfType::KDF_ID.to_be_bytes().as_slice(),
-    //     AeadType::AEAD_ID.to_be_bytes().as_slice(),
-    // ].concat();
+    let hdr = [
+        d.to_be_bytes().as_slice(),
+        KemType::KEM_ID.to_be_bytes().as_slice(),
+        KdfType::KDF_ID.to_be_bytes().as_slice(),
+        AeadType::AEAD_ID.to_be_bytes().as_slice(),
+    ].concat();
 
     // info = concat(encode_str("message/bhttp request"),
     //               encode(1, 0),
     //               HDR)
-    // let info = ["message/bhttp request".as_bytes(), &[0u8; 1], &HDR].concat();
+    let info = ["message/bhttp request".as_bytes(), &[0u8; 1], &hdr].concat();
 
-    let pkr = <KemType as KemTrait>::PublicKey::from_bytes(&PKR_BYTES).unwrap();
+    let pkr = <KemType as KemTrait>::PublicKey::from_bytes(r).unwrap();
 
     let mode = if i.len() > 0 {
         OpModeS::Psk(PskBundle { psk: k, psk_id: i })
@@ -55,7 +51,7 @@ pub fn s( // s for seal
     };
 
     // enc, sctxt = SetupBaseS(pkR, info)
-    let (enc, mut sender_ctx) = hpke::setup_sender::<AeadType, KdfType, KemType, _>(&mode, &pkr, &INFO, &mut rng).unwrap();
+    let (enc, mut sender_ctx) = hpke::setup_sender::<AeadType, KdfType, KemType, _>(&mode, &pkr, &info, &mut rng).unwrap();
 
     // ct = sctxt.Seal("", request)
     let mut pt_vec = p.to_vec();
@@ -69,7 +65,7 @@ pub fn s( // s for seal
 
     // enc_request = concat(hdr, enc, ct)
     // result = secret || HDR || enc || ciphertext || tag
-    [secret.as_slice(), HDR.as_slice(), enc.to_bytes().as_slice(), ciphertext.as_slice(), tag.to_bytes().as_slice()].concat()
+    [secret.as_slice(), hdr.as_slice(), enc.to_bytes().as_slice(), ciphertext.as_slice(), tag.to_bytes().as_slice()].concat()
 }
 
 #[wasm_bindgen]
